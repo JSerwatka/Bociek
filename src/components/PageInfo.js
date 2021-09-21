6eb6c9302658bfa219af11662c56eb037d0796c6
Jakub Serwatka <kubaserw@gmail.com>
2021-09-21 12:18:36 +0200

    feat: add credits to page info

--- src/components/PageInfo.js
+++ src/components/PageInfo.js
@@ -48,12 +48,22 @@ function PageInfo() {
                             </div>
                         </div>
                         <div className="contribution-info">
-                            <div className="contribution-title">Icons</div>
-                            All icons made by <a href="https://www.freepik.com" title="Freepik" target="_blank" rel="noopener noreferrer">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon" target="_blank" rel="noopener noreferrer">www.flaticon.com</a>.
+                            <div className="contribution-title">Icons and animations</div>
+                            <ul className="info-list">
+                                <li>
+                                    All icons made by <a href="https://www.freepik.com" title="Freepik" target="_blank" rel="noopener noreferrer">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon" target="_blank" rel="noopener noreferrer">www.flaticon.com</a>.
+                                </li>
+                                <li>
+                                    Loading screen weather animation based on <a href="https://codepen.io/code4food/pen/rLvggd" target="_blank" rel="noopener noreferrer">codepen</a> by <a href="https://codepen.io/code4food" target="_blank" rel="noopener noreferrer">Tuan Hoang</a>
+                                </li>
+                                <li>
+                                    Loading screen dots animation based on <a href="https://codepen.io/jakob-e/pen/mKceA" target="_blank" rel="noopener noreferrer">codepen</a> by <a href="https://codepen.io/jakob-e" target="_blank" rel="noopener noreferrer">jakob-e</a>
+                                </li>
+                            </ul>
                         </div>
                         <div className="contribution-info">
                             <div className="contribution-title">Other awesome tools I've used</div>
-                            <ul className="tools-list">
+                            <ul className="info-list">
                                 <li>
                                     For goejson data processing: <a href="https://turfjs.org/" target="_blank" rel="noopener noreferrer">turf</a>
                                 </li>
