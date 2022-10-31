48ac7edaa80c8d7b48862b32a6c1d1ea8ccdb655
Jakub Serwatka <kubaserw@gmail.com>
2022-10-31 11:23:37 +0100

    feat: init new vite + ts project

---
+++ vite-ver/bociek/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div className="min-h-screen flex justify-center items-center">
+            <h1 className="text-3xl font-bold text-blue-600">
+                Install & Setup Vite + React + Typescript + Tailwind CSS 3
+            </h1>
+        </div>
+    );
+}
+
+export default App;
