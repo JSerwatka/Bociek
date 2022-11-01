d70bfd8c04e0cc6bcbc32fbc7124c32cb1bc8633
Jakub Serwatka <kubaserw@gmail.com>
2022-11-01 09:07:44 +0100

    refactor: move changing layers to a seperate component

--- vite-ver/bociek/src/components/Map/Map.tsx
+++ vite-ver/bociek/src/components/Map/Map.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from "react";
-import { MapContainer, LayersControl, TileLayer, GeoJSON } from "react-leaflet";
+import { MapContainer, GeoJSON } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 import "../../styles/Map/map.css";
@@ -11,6 +11,7 @@ import { getHoursFromTime } from "../../utils/hoursFromTime";
 import { fetchData } from "../../utils/fetchData";
 import { DataType, MonthsType } from "../../types/commonTypes";
 import { map } from "leaflet";
+import LayerChoice from "./segments/LayersChoice";
 
 // #TODO fix types
 interface MapProps {
@@ -206,34 +207,7 @@ const Map = ({ month, dataType, worldGeojson, airTemp, precipitation, dayLength 
             ]}
             ref={mapRef}
         >
-            <LayersControl position="topright">
-                <LayersControl.BaseLayer checked name="OpenStreetMap.Mapnik">
-                    <TileLayer
-                        attribution='&copy; <a href="http://osm.org/copyright" target="_blank" rel="noopener noreferrer">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                </LayersControl.BaseLayer>
-                <LayersControl.BaseLayer name="Stamen.Watercolor">
-                    <TileLayer
-                        attribution='Map tiles by <a href="http://stamen.com" target="_blank" rel="noopener noreferrer">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0" target="_blank" rel="noopener noreferrer">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noopener noreferrer">OpenStreetMap</a> contributors'
-                        url="https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}"
-                        subdomains="abcd"
-                        ext="jpg"
-                    />
-                </LayersControl.BaseLayer>
-                <LayersControl.BaseLayer name="Esri.NatGeoWorldMap">
-                    <TileLayer
-                        attribution="Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC"
-                        url="https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}"
-                    />
-                </LayersControl.BaseLayer>
-                <LayersControl.BaseLayer name="OpenTopoMap">
-                    <TileLayer
-                        attribution='Map data: &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noopener noreferrer">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org" target="_blank" rel="noopener noreferrer">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org" target="_blank" rel="noopener noreferrer">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/" target="_blank" rel="noopener noreferrer">CC-BY-SA</a>)'
-                        url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
-                    />
-                </LayersControl.BaseLayer>
-            </LayersControl>
+            <LayerChoice />
             <GeoJSON data={worldGeojson} style={mapStyles} onEachFeature={onEachDivision} />
             <Legend dataType={dataType} />
         </MapContainer>
