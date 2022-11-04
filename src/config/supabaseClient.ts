c211639cd41bee3e00a6f1084cb064ef1dce4dfd
Jakub Serwatka <kubaserw@gmail.com>
2022-11-04 21:03:15 +0100

    feat: add supabase client config

---
+++ src/config/supabaseClient.ts
@@ -0,0 +1,12 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+    throw new Error("No supabase key or url");
+}
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase
