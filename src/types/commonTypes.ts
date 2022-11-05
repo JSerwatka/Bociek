a9d2c50fd6a29a8f0b23890800223eb944f90d06
Jakub Serwatka <kubaserw@gmail.com>
2022-11-05 14:45:55 +0100

    refactor: use different naming for gloval data types

--- src/types/commonTypes.ts
+++ src/types/commonTypes.ts
@@ -1,3 +1,3 @@
-export type DataType = "temp" | "rain" | "daylength";
+export type DataType = "max_temp" | "precipitation" | "day_length";
 
 export type MonthsType = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11;
